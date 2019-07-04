b5dc392033ac751a17f83b7c9e4f3dc1e11968e5
Git_test <away1597@gmail.com>
2019-07-04 15:43:25 +0900

    testing

--- postProcessor.cs
+++ postProcessor.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class postProcessor : MonoBehaviour
 {
+    public Text text;
+
     // Start is called before the first frame update
     void Start()
     {
         
+#if UNITY_EDITOR_WIN
+        text.text += "Unity_editor";
+#elif UNITY_ANDROID
+        text.text += "Unity_android";
+#endif
     }
 
     // Update is called once per frame
